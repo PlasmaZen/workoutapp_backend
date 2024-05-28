c3e20ddc27a5c3af7634ede4df64f5ad890161f9
br <31518912+PlasmaZen@users.noreply.github.com>
2024-05-28 13:03:51 -0600

    sort of got the program to interface with mongodb. super broken. on mongoatlas it shows a connection but no collections

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-const express = require('express')
-const mongoose = require('mongoose'); 
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require('./models/product.models.js')
+const app = express();
+
+app.use(express.json)
 
 app.listen(5000, () => {
-    console.log('Server is running on port 5000')
+  console.log("Server is running on port 5000");
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello from Node API');
+app.get("/", (req, res) => {
+  res.send("Hello from Node API");
 });
 
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
-mongoose.connect("mongodb+srv://plasmazen:ESVIyPWGDQLTJMXf@statsapptest.f3kco8v.mongodb.net/?retryWrites=true&w=majority&appName=statsapptest")
-.then(() => {
+mongoose
+  .connect(
+    "mongodb+srv://plasmazen:M8fG1cSsdFF2AbwX@statsapptest.f3kco8v.mongodb.net/?retryWrites=true&w=majority&appName=statsapptest"
+  )
+  .then(() => {
     console.log("Connected to database!");
-})
-.catch(() => {
+  })
+  .catch(() => {
     console.log("Connection failed!");
-});
+  });
--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
 https://www.youtube.com/watch?v=_7UQPve99r4
+
+if you get nodemon crashes type pkill -f node in terminal
